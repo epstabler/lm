84225a6d39409bc973b3b3d15932893d4dd00ed3
es <edwardstabler@urqu.local>
2023-11-05 06:21:40 -0800

    initial

--- 1/corpora/BUILD-CORPORA.py
+++ 1/corpora/BUILD-CORPORA.py
@@ -1,7 +1,10 @@
 import sys, re, nltk, pyconll, epitran
 
+EPI_PATH = "/Users/es/.local/src/epitran/"
+CEDICT_PATH = EPI_PATH + "cedict_1_0_ts_utf-8_mdbg.txt"
+
 USAGE = """
-This code reads specified files from the ud-treebanks,
+This code reads specified files from selected nltk- and ud-treebanks,
 translates them to ipa, and writes out the results in various formats.
 
 Use:
@@ -10,9 +13,9 @@ Use:
 
 where each LANGUAGEi is one of these:
 
- cmn eng emma fra hin pol rus spa tha
+   cmn eng fra hin pol rus spa tha
 
-(those are: Mandarin, English, JaneAutensEnglishNovel-Emma, French, Hindi, Polish, Russian, Spanish, Thai)
+(those are: Mandarin, English, French, Hindi, Polish, Russian, Spanish, Thai)
 
 Alternatively:
 
@@ -20,39 +23,39 @@ Alternatively:
 
 builds corpora for all 8 languages.
 
-( It is easy to extend this script to use additional UD corpora.
-These languages chosen because also covered fairly well by epitran. )
+( These languages chosen because also covered fairly well by epitran. 
+  It is easy to extend this script to use additional corpora.
+)
 
-On my laptop, building all corpora, this script takes some time to run!
+On laptops and other small computers, building these corpora takes some time,
+  esp. for the larger ones like eng and cmn ... time for a coffee break.
 """
 
-def ud(lang):
+def cors(lang):
+    """ corpora paths for ud + names for nltk """
     if lang == 'cmn':    
-        return 'ud-treebanks-v2.12/UD_Chinese-GSD/zh_gsd-ud-train.conllu'
-    elif lang == 'emma':
-        return 'emma'
+        return ['ud-treebanks-v2.12/UD_Chinese-GSD/zh_gsd-ud-train.conllu']
     elif lang == 'eng':
-        return 'ud-treebanks-v2.12/UD_English-EWT/en_ewt-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_English-EWT/en_ewt-ud-train.conllu', 'emma']
     elif lang == 'fra':
-        return 'ud-treebanks-v2.12/UD_French-GSD/fr_gsd-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_French-GSD/fr_gsd-ud-train.conllu']
     elif lang == 'hin':
-        return 'ud-treebanks-v2.12/UD_Hindi-HDTB/hi_hdtb-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_Hindi-HDTB/hi_hdtb-ud-train.conllu']
     elif lang == 'pol':
-        return 'ud-treebanks-v2.12/UD_Polish-PDB/pl_pdb-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_Polish-PDB/pl_pdb-ud-train.conllu']
     elif lang == 'rus':
-        return 'ud-treebanks-v2.12/UD_Russian-SynTagRus/ru_syntagrus-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_Russian-SynTagRus/ru_syntagrus-ud-train.conllu']
     elif lang == 'spa':
-        return 'ud-treebanks-v2.12/UD_Spanish-AnCora/es_ancora-ud-train.conllu'
+        return ['ud-treebanks-v2.12/UD_Spanish-AnCora/es_ancora-ud-train.conllu']
     elif lang == 'tha':
-        return 'ud-treebanks-v2.12/UD_Thai-PUD/th_pud-ud-test.conllu'
+        return ['ud-treebanks-v2.12/UD_Thai-PUD/th_pud-ud-test.conllu']
     else:
-        raise ValueError('ud lang name error')
+        raise ValueError('uds lang name error')
 
 def ep(lang):
+    """ epitran models """
     if lang == 'cmn':
-        return epitran.Epitran('cmn-Hant', cedict_file="/Users/es/.local/src/epitran/cedict_1_0_ts_utf-8_mdbg.txt")
-    elif lang == 'emma':
-        return epitran.Epitran('eng-Latn')
+        return epitran.Epitran('cmn-Hant', cedict_file=CEDICT_PATH)
     elif lang == 'eng':
         return epitran.Epitran('eng-Latn')
     elif lang == 'fra':
@@ -70,24 +73,26 @@ def ep(lang):
     else:
         raise ValueError('Epitran model name error')
 
-def udName(lang):
-    if lang == 'emma':
+def udName(lang, cor):
+    """ create simple language-corpus prefix for names of created files """
+    if cor == 'emma':
         return 'eng-emma'
     else:
-        pathParts = ud(lang).split('/')
+        pathParts = cor.split('/')
         udf = pathParts[-1].split('-')
         udfParts = udf[0].split('_')
         if len(udfParts) > 1:
             return '-'.join([lang,udfParts[1]])
         else:
-            raise ValueError('UD corpus name format error: %s' % lang)
+            raise ValueError('UD corpus name format error: %s' % cor)
 
 VERBOSE = False
 
 def adjustTxt(lang, txt):
-    """ text adjustments, specialized to each corpus """
+    """ minor text adjustments, specialized to each corpus,
+         since we're interested in pronunciations...
+    """
     if lang == 'cmn':
-        # since we're interested in pronunciations...
         # replace punctuation with space:
         for ch in ['一', '。', '？', '：', '，', '；', '》', '《',
                    '\)', '\(', '）', '（', '〔', '〕', '！', '$',
@@ -102,7 +107,7 @@ def adjustTxt(lang, txt):
         txt = re.sub(" ' s ", "'s ", txt)   # keep possessive 's attached, for emma
         txt = re.sub('Dr\.', 'Doctor', txt) # epitran does not understand "Dr.", for emma
         txt = re.sub('&c', 'etcetera', txt) # epitran does not understand "&c", for emma
-        # fix some minor (rare) things that might appear in examples
+        # fix some (rare) things that might appear in examples
         txt = re.sub('Dr\.', 'Doctor', txt)
         txt = re.sub('Mr\.', 'Mister', txt)
         txt = re.sub('Mrs\.', 'Missus', txt)
@@ -217,10 +222,11 @@ def adjustTxt(lang, txt):
         raise ValueError('adjustText lang error: %s' % lang)
 
 def adjustIPA(ipa):
+    """ stylistic adjustments in epitran IPA """
     # represent syllabic r with schwa, as epitran does for syllabic l
+    ipa = re.sub('ɹ̩','əɹ',ipa)   # printer -> pɹɪntɹ̩ -> pɹɪntəɹ
     ipa = re.sub('ɻ̩','əɻ',ipa)
     ipa = re.sub('r̩','ər',ipa)
-    ipa = re.sub('ɹ̩','əɹ',ipa)   # printer -> pɹɪntɹ̩ -> pɹɪntəɹ
     # remove tie bars
     ipa = re.sub('t͡ʃ','tʃ',ipa)
     ipa = re.sub('t͡s','ts',ipa)
@@ -232,7 +238,7 @@ def adjustIPA(ipa):
     ipa = re.sub('d͡z','dz',ipa)
     ipa = re.sub('d͡ʐ','dʐ',ipa)
     ipa = re.sub('d͡s','ds',ipa)
-    # adjust epitran representations of common English diphthongs
+    # adjust epitran representations of some diphthongs
     ipa = re.sub('oj','ɔɪ',ipa)  # boy -> boj -> bɔɪ
     ipa = re.sub('ow','oʊ',ipa)  # home -> howm -> hoʊm
     ipa = re.sub('aw','aʊ',ipa)  # loud -> lawd -> laʊd
@@ -243,72 +249,76 @@ def adjustIPA(ipa):
 def buildCorpus(langs):
 
     for lang in langs:
-        udn = udName(lang)
-        print('building %s in various formats...' % udn)
 
-        if lang == 'emma':
-            corpusListed = nltk.Text(nltk.corpus.gutenberg.sents('austen-emma.txt'))
-            corpus = [' '.join(s) for s in corpusListed]
-        else:
-            corpusListed = pyconll.load_from_file(ud(lang))
-            corpus = [sentence.text for sentence in corpusListed]
+        for cor in cors(lang):
 
-        epi = ep(lang)
-    
-        ss = open('%s-sentences.txt' % udn, 'w')
-    
-        ws = open('%s-words.txt' % udn, 'w')
-        wsa = open('%s-wordsSpaced.txt' % udn, 'w')
-    
-        ps = open('%s-ipa.txt' % udn, 'w')
-        psa = open('%s-ipaSpaced.txt' % udn, 'w') 
-    
-        cCnt = 0
+            udn = udName(lang, cor)
 
-        for st in corpus:
-            if not('[' in st) and \
-               not('http' in st) and \
-               not('VOLUME' in st) and \
-               not('CHAPTER' in st):
- 
-                ss.write(st)
-                ss.write('\n')
-                if VERBOSE:
-                    print('snt =',st)
-                else:
-                    cCnt += len(st)
-                    sys.stdout.write('\r%d characters processed' % cCnt)
-                    sys.stdout.flush()
+            print('building %s in various formats...' % udn)
     
-                txt = adjustTxt(lang,st)
-                if VERBOSE: print('txt =',txt)
-                ws.write(txt)
-                ws.write('\n')
-                for x in txt:
-                    if x != ' ':
-                        wsa.write(x)
-                        wsa.write(' ')
+            if cor == 'emma':
+                corpusListed = nltk.Text(nltk.corpus.gutenberg.sents('austen-emma.txt'))
+                corpus = [' '.join(s) for s in corpusListed]
+            else:
+                corpusListed = pyconll.load_from_file(cor)
+                corpus = [sentence.text for sentence in corpusListed]
+    
+            epi = ep(lang)
+        
+            ss = open('%s-sentences.txt' % udn, 'w')
+        
+            ws = open('%s-words.txt' % udn, 'w')
+            wsa = open('%s-wordsSpaced.txt' % udn, 'w')
+        
+            ps = open('%s-ipa.txt' % udn, 'w')
+            psa = open('%s-ipaSpaced.txt' % udn, 'w') 
+        
+            cCnt = 0
+    
+            for st in corpus:
+                if not('[' in st) and \
+                   not('http' in st) and \
+                   not('VOLUME' in st) and \
+                   not('CHAPTER' in st):
+     
+                    ss.write(st)
+                    ss.write('\n')
+                    if VERBOSE:
+                        print('snt =',st)
                     else:
-                        wsa.write('_ ')
-                wsa.write('\n')
-    
-                ipa = adjustIPA(epi.transliterate(txt))
-                if VERBOSE: print('ipa =',ipa)
-                ps.write(ipa)
-                ps.write('\n')
-                for x in ipa:
-                    if x != ' ':
-                        psa.write(x)
-                        psa.write(' ')
-                    else:
-                        psa.write('_ ')
-                psa.write('\n')
-        sys.stdout.write('\n%s transcription complete\n' % lang)
+                        cCnt += len(st)
+                        sys.stdout.write('\r%d characters processed' % cCnt)
+                        sys.stdout.flush()
+        
+                    txt = adjustTxt(lang,st)
+                    if VERBOSE: print('txt =',txt)
+                    ws.write(txt)
+                    ws.write('\n')
+                    for x in txt:
+                        if x != ' ':
+                            wsa.write(x)
+                            wsa.write(' ')
+                        else:
+                            wsa.write('_ ')
+                    wsa.write('\n')
+        
+                    ipa = adjustIPA(epi.transliterate(txt))
+                    if VERBOSE: print('ipa =',ipa)
+                    ps.write(ipa)
+                    ps.write('\n')
+                    for x in ipa:
+                        if x != ' ':
+                            psa.write(x)
+                            psa.write(' ')
+                        else:
+                            psa.write('_ ')
+                    psa.write('\n')
+            sys.stdout.write('\n%s transcription complete\n' % lang)
 
 if __name__ == '__main__':
     if not len(sys.argv) > 1:
         print(USAGE)
     elif sys.argv[1:] == ['all']:
-        buildCorpus(['cmn', 'emma', 'eng', 'fra', 'hin', 'pol', 'rus', 'spa', 'tha'])
+        buildCorpus(['cmn', 'eng', 'fra', 'hin', 'pol', 'rus', 'spa', 'tha'])
     else:
         buildCorpus(sys.argv[1:])
